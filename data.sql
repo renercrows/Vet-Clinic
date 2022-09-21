af2f63a54a29fa518ef082807650b086dd7c137b
renercrows <ungredda95@gmail.com>
2022-09-21 17:17:57 -0400

    Add Tables, Data, Questions and Screenshots

--- data.sql
+++ data.sql
@@ -9,10 +9,31 @@ INSERT INTO animal (name, date_of_birth, escape_attempts, neutered, weight_kg) V
 
 /* Tuesday */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', 'February 8, 2020', 0, false, '-11');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon', 'November 15, 2021', 2, true, '-5.7');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', 'April 2, 1993', 3, false, '-12.13');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', 'June 12, 2005', 1, true, '-45');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', 'June 7, 2005', 7, true, '20.4');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', 'October 13, 1998', 3, true, '17');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', 'May 14, 2022', 4, true, '22');
+INSERT INTO animal (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', 'February 8, 2020', 0, false, '-11');
+INSERT INTO animal (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon', 'November 15, 2021', 2, true, '-5.7');
+INSERT INTO animal (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', 'April 2, 1993', 3, false, '-12.13');
+INSERT INTO animal (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', 'June 12, 2005', 1, true, '-45');
+INSERT INTO animal (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', 'June 7, 2005', 7, true, '20.4');
+INSERT INTO animal (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', 'October 13, 1998', 3, true, '17');
+INSERT INTO animal (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', 'May 14, 2022', 4, true, '22');
+
+/* Wednesday */
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
+UPDATE animal SET species_id = 1 WHERE name LIKE '%mon';
+UPDATE animal SET species_id = 2 WHERE name NOT LIKE '%mon';
+
+UPDATE animal SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animal SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animal SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animal SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animal SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
--- queries.sql
+++ queries.sql
@@ -14,7 +14,6 @@ SELECT * FROM animal WHERE weight_kg BETWEEN 10.4 AND 17.3;
 /* Tuesday */
 
 BEGIN;
-/* update species column to unspecified for all animal */
 UPDATE animal
 SET species = 'unspecified';
 ROLLBACK;
@@ -29,7 +28,6 @@ WHERE name NOT LIKE '%mon';
 COMMIT;
 
 BEGIN;
-/* delete all records in the animal table, then roll back the transaction */
 DELETE FROM animal;
 ROLLBACK;
 
@@ -52,3 +50,13 @@ SELECT AVG(weight_kg) FROM animal;
 SELECT neutered, AVG(escape_attempts) AS average_escapes FROM animal GROUP BY neutered;
 SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animal GROUP BY species;
 SELECT AVG(escape_attempts) FROM animal WHERE date_of_birth BETWEEN 'January 1, 1990' AND ' December 31, 2000';
+
+/* Wednesday */
+
+SELECT name FROM animals INNER JOIN owners ON owner_id = owners.id WHERE full_name = 'Melody Pond';
+SELECT animals.name FROM animals INNER JOIN species ON species_id = species.id WHERE species.name = 'Pokemon';
+SELECT animals.name, owners.full_name FROM animals RIGHT JOIN owners ON animals.owner_id = owners.id;
+SELECT COUNT(*), species.name AS species FROM animals JOIN species ON species_id = species.id GROUP BY species.name;
+SELECT animals.name AS digimons_name, owners.full_name AS owner_name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE animals.species_id = 2 AND owners.full_name = 'Jennifer Orwell';
+SELECT animals.name, owners.full_name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE animals.escape_attempts = 0 AND owners.full_name = 'Dean Winchester';
+SELECT full_name, COUNT(full_name) AS owns FROM animals JOIN owners ON animals.owner_id = owners.id GROUP BY full_name ORDER BY owns DESC LIMIT 1;
